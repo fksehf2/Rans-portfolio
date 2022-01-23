fded0c14fe47c2456f4876e9d73312234be3a886
fksehf2 <fkstpdl2@gmail.com>
2022-01-23 22:00:21 +0900

    js scroll

--- main.js
+++ main.js
@@ -12,3 +12,30 @@ document.addEventListener('scroll', ()=> {
     navbar.classList.remove('navbar--dark');
   }
 })
+
+//navbar scrolling
+
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+  //console.log(event.target.dataset.link);
+  const target = event.target;
+  const link= target.dataset.link;
+  if(link == null){
+    return; 
+  }
+
+  //console.log(event.target.dataset.link);
+  scrollIntoView(link);
+});
+
+//contact me move
+const contact = document.querySelector('.home_contact');
+contact.addEventListener('click', (event)=>{
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector){
+  const scrollTo=document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+
